3d6e213e1e3c7f23c4947e36df90c6c05e0ba615
rnaseri14012-maker <r.naseri14012@gmail.com>
2026-08-25 22:35:32 +0330

    bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,26 @@
 require("dotenv").config();
 
 const { Telegraf } = require("telegraf");
+const http = require("http");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+// صفحه ساده برای Render
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    "Content-Type": "text/plain; charset=utf-8"
+  });
+
+  res.end("Anonymous Chat Bot is running 🤖");
+});
+
+server.listen(PORT, () => {
+  console.log(`🌐 Web server running on port ${PORT}`);
+});
+
+// شروع ربات
 bot.start((ctx) => {
   ctx.reply(
     "👋 سلام!\n\n" +
@@ -12,6 +29,7 @@ bot.start((ctx) => {
   );
 });
 
+// راهنما
 bot.command("help", (ctx) => {
   ctx.reply(
     "ℹ️ راهنما\n\n" +
@@ -19,9 +37,11 @@ bot.command("help", (ctx) => {
   );
 });
 
+// اجرای ربات
 bot.launch();
 
 console.log("🤖 Anonymous Chat Bot started");
 
+// خاموش شدن صحیح
 process.once("SIGINT", () => bot.stop("SIGINT"));
 process.once("SIGTERM", () => bot.stop("SIGTERM"));
